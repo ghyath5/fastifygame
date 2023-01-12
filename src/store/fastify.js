1a8e983ad232d442b3ab9a54b7dabe12c65bbb6b
Ghyath <ghyath.darwish@gmail.com>
2023-01-12 16:43:15 +0200

    add time

--- src/store/fastify.js
+++ src/store/fastify.js
@@ -1,6 +1,6 @@
 import { ref, reactive } from "vue";
 
-export const TIME_LIMIT = 50;
+export const TIME_LIMIT = 60;
 export const lastScore = ref(0)
 export const time = ref(TIME_LIMIT);
 export const activeWordIndex = ref(0);
